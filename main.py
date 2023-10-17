3db2b0f4fd99b54174ba2cf50b1dffb36b44cbf9
ethanjiang <ethanjiang666@163.com>
2023-10-17 10:46:14 +0800

    readme and main code changes

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
 import uvicorn
-from class_info.lessons.opening_file import Helper
+from fastapi.security import api_key
 
 app = FastAPI()
-data = Helper()
 
-# First we make some fake data to be able to use as our "database calls"
+API_KEYS = ["060501", "060212"]
+
 class_pass_chance = [
     {"Participant": "a", "chance to fail the class": 0.100, "age": 18},
     {"Participant": "b", "chance to fail the class": 0.110, "age": 17},
@@ -31,23 +31,49 @@ class_pass_chance = [
 ]
 
 
-@app.get("/class_pass_chance/intro")
-async def get_me():
-    return {"participant": "chance to fail the class"}
+@app.get("/class_pass_chance")
+async def names(api_key: str):
+    if api_key in API_KEYS:
+        res = []
+        for participant_names in class_pass_chance:
+            res.append(participant_names['Participant'])
+        return res
+    return {"msg": "what are you doing here? You don't have permission"}
+
+
+@app.get("/class_pass_chance/{participant_name}/fail_chance")
+async def get_chance(participant_name: str, api_key: str):
+    print(api_key, participant_name)
+    if api_key in API_KEYS:
+        for name_list in class_pass_chance:
+            print(participant_name.lower(), name_list["Participant"])
+            if participant_name.lower() == name_list["Participant"]:
+                return f"The participant {name_list['Participant']} has a fail chance of {name_list['chance to fail the class']}."
+        return f"who are you looking for? Type again."
+    return {"msg": "You don't have the right to access the information."}
 
 
 
-@app.get("/class_pass_chance/{Participant}")
-async def get_profile(participant_name: str):
-    for participant in class_pass_chance:
-        if participant["Participant"].lower() == participant_name.lower():
-            return participant
-    return {"msg": "User not found"}
+@app.get("/class_pass_chance/{participant_name}/age")
+async def get_age(participant_name: str, api_key: str):
+    print(api_key)
+    if api_key in API_KEYS:
+        for name_list in class_pass_chance:
+            if participant_name.lower() == name_list["Participant"]:
+                return f"The participant {name_list['Participant']} is {name_list['age']} years old."
+        return f"who are you looking for? Type again."
+    return {"msg": "You don't have the right to access the information."}
 
 
+@app.get("/class_pass_chance/get_key")
+async def get_key():
+    return f"api key are.... why do you think I would tell you?"
 
-@app.get("/number of participants involved")
-async def get_secret():
-    return {"msg": "There are 20 participants."}
+@app.get("/class_pass_chance/info")
+async def intro(api_key: str):
+    if api_key in API_KEYS:
+        return("Hello there! this is about the class fail chance in Gamedesign 3!")
+    return f"nah man. You probably in the wrong place. Else, check the api_key!"
+
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", reload=True)
